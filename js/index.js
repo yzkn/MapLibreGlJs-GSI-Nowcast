9eecf0d4ab5f53f271cd1465ad6862603a7207be
YA <9039417+yzkn@users.noreply.github.com>
2023-09-08 09:39:37 +0900

    Add Export button

--- js/index.js
+++ js/index.js
@@ -7,3 +7,26 @@ const map = new maplibregl.Map({
     center: [139.7109, 35.729503],
     zoom: 10
 });
+
+map.addControl(new maplibregl.NavigationControl(), 'bottom-right');
+
+map.addControl(new maplibregl.ScaleControl());
+
+// Export
+map.addControl(new MaplibreExportControl.MaplibreExportControl({
+    Format: MaplibreExportControl.Format.PNG,
+    DPI: MaplibreExportControl.DPI[400]
+}), 'bottom-right');
+// Export
+
+
+// map.addControl(
+//     new maplibregl.GeolocateControl({
+//         positionOptions: {
+//             enableHighAccuracy: true
+//         },
+//         trackUserLocation: true,
+//         showUserHeading: true
+//     }),
+//     'bottom-right'
+// );
