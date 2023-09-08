bbf4b90ac13eaef549788bbe187cebba23a9f2c3
YA <9039417+yzkn@users.noreply.github.com>
2023-09-08 09:26:03 +0900

    init

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 YA-androidapp(https://github.com/yzkn) All rights reserved.
+
+
+const map = new maplibregl.Map({
+    container: 'map',
+    style: 'https://demotiles.maplibre.org/style.json',
+    center: [139.7109, 35.729503],
+    zoom: 10
+});
